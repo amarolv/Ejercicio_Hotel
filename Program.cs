21b004e9cfc23db566637b88e85cc1085efc1baf
AmaroLV <amaro.leal.v@gmail.com>
2020-10-01 12:08:33 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Ejercicio_Hotel
+{
+    class Program
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["DDBB_HOTEL"].ConnectionString;
+        static SqlConnection conexion = new SqlConnection(connectionString);
+        //static string cadena;
+        //static SqlCommand comando;
+        //static SqlDataReader registros;
+
+        static void Main(string[] args)
+        {
+            conexion.Open();
+            string query = "SELECT * FROM Habitaciones";
+            SqlCommand comando = new SqlCommand(query, conexion);
+            SqlDataReader registros = comando.ExecuteReader();
+            while (registros.Read())
+            {
+                Console.WriteLine(registros["CodHabitacion"].ToString() + "\n" +
+                    "\t" + registros["Estado"]);
+            }
+        }
+    }
+}
